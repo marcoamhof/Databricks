180c61bf7b4773580afac19a4aa588f0a65db890
marcoamhof <marco.amhof@trivadis.com>
2018-10-27 10:33:38 +0200

    Update: .gitignore README.md notebooks

---
+++ notebooks/SendTweetsToEventHub.scala
@@ -0,0 +1,70 @@
+// Databricks notebook source
+import java.util._
+import scala.collection.JavaConverters._
+import com.microsoft.azure.eventhubs._
+import java.util.concurrent._
+
+val namespaceName = "amhStreamingDemo"
+val eventHubName = "twitterstream"
+val sasKeyName = "allpolicies"
+val sasKey = "qG9Rct84L/HYTlJCzq/dqpmvQyYNFi4k8hGZz4Cdm0k="
+val connStr = new ConnectionStringBuilder()
+            .setNamespaceName(namespaceName)
+            .setEventHubName(eventHubName)
+            .setSasKeyName(sasKeyName)
+            .setSasKey(sasKey)
+
+val pool = Executors.newFixedThreadPool(1)
+val eventHubClient = EventHubClient.create(connStr.toString(), pool)
+
+def sendEvent(message: String) = {
+  val messageData = EventData.create(message.getBytes("UTF-8"))
+  eventHubClient.get().send(messageData)
+  System.out.println("Sent event: " + message + "\n")
+}
+
+import twitter4j._
+import twitter4j.TwitterFactory
+import twitter4j.Twitter
+import twitter4j.conf.ConfigurationBuilder
+
+// Twitter configuration!
+// Replace values below with yours
+
+val twitterConsumerKey = "mwgJTYabMBsMjBU0NvrRFqUwZ"
+val twitterConsumerSecret = "pVR98eKTr2PMdR5adizfyGV8RNAeEbNuWYOdaVwlgtoAlho97C"
+val twitterOauthAccessToken = "4912497401-AxkbXHssMa45CzCRdI83pouFCAZaUE3wViGMSGL"
+val twitterOauthTokenSecret = "aiLXpYxxHGcZTedkpM9BY8nkITTToWStiK3xhTDumH6FA"
+
+val cb = new ConfigurationBuilder()
+  cb.setDebugEnabled(true)
+  .setOAuthConsumerKey(twitterConsumerKey)
+  .setOAuthConsumerSecret(twitterConsumerSecret)
+  .setOAuthAccessToken(twitterOauthAccessToken)
+  .setOAuthAccessTokenSecret(twitterOauthTokenSecret)
+
+val twitterFactory = new TwitterFactory(cb.build())
+val twitter = twitterFactory.getInstance()
+
+// Getting tweets with keyword "Azure" and sending them to the Event Hub in realtime!
+
+val query = new Query(" #blackfriday ")
+query.setCount(1000)
+query.lang("en")
+var finished = false
+while (!finished) {
+  val result = twitter.search(query)
+  val statuses = result.getTweets()
+  var lowestStatusId = Long.MaxValue
+  for (status <- statuses.asScala) {
+    if(!status.isRetweet()){
+      sendEvent(status.getText())
+    }
+    lowestStatusId = Math.min(status.getId(), lowestStatusId)
+    Thread.sleep(2000)
+  }
+  query.setMaxId(lowestStatusId - 1)
+}
+
+// Closing connection to the Event Hub
+eventHubClient.get().close()
