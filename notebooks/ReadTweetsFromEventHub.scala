dc1f8da9a07a0a1cbf49aa3c6df40749545497e6
marcoamhof <marco.amhof@trivadis.com>
2018-10-27 10:21:42 +0200

    First check in

---
+++ notebooks/ReadTweetsFromEventHub.scala
@@ -0,0 +1,39 @@
+// Databricks notebook source
+import org.apache.spark.eventhubs._
+
+    // Build connection string with the above information
+    val connectionString = ConnectionStringBuilder("Endpoint=sb://amhstreamingdemo.servicebus.windows.net/;SharedAccessKeyName=allpolicies;SharedAccessKey=qG9Rct84L/HYTlJCzq/dqpmvQyYNFi4k8hGZz4Cdm0k=;EntityPath=twitterstream")
+      .setEventHubName("twitterstream")
+      .build
+
+    val customEventhubParameters =
+      EventHubsConf(connectionString)
+      .setMaxEventsPerTrigger(5)
+
+    val incomingStream = spark.readStream.format("eventhubs").options(customEventhubParameters.toMap).load()
+
+    incomingStream.printSchema
+
+    // Sending the incoming stream into the console.
+    // Data comes in batches!
+    incomingStream.writeStream.outputMode("append").format("console").option("truncate", false).start().awaitTermination()
+
+
+// COMMAND ----------
+
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+
+// Event Hub message format is JSON and contains "body" field
+// Body is binary, so we cast it to string to see the actual content of the message
+val messages =
+    incomingStream
+      .withColumn("Offset", $"offset".cast(LongType))
+      .withColumn("Time (readable)", $"enqueuedTime".cast(TimestampType))
+      .withColumn("Timestamp", $"enqueuedTime".cast(LongType))
+      .withColumn("Body", $"body".cast(StringType))
+      .select("Offset", "Time (readable)", "Timestamp", "Body")
+
+messages.printSchema
+
+messages.writeStream.outputMode("append").format("console").option("truncate", false).start().awaitTermination()
