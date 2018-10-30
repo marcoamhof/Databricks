a70e85c2c8eeb383a0ca360a84269aaa31495a1b
marcoamhof <marco.amhof@trivadis.com>
2018-10-30 14:46:02 +0100

    Ready for Demo ;-)

--- notebooks/Users/marco.amhof@trivadis.com/triCast 201810/triCast Azure Databricks in 5 minutes.sql
+++ notebooks/Users/marco.amhof@trivadis.com/triCast 201810/triCast Azure Databricks in 5 minutes.sql
@@ -11,7 +11,7 @@
 -- MAGIC 
 -- MAGIC 1. In the sidebar, right-click the **Clusters** button and open the link in a new window.
 -- MAGIC 1. On the Clusters page, click **Create Cluster**.
--- MAGIC 1. Name the cluster **QuickstartDemo**.
+-- MAGIC 1. Name the cluster **QuickStartDemo**.
 -- MAGIC 1. In the Databricks Runtime Version drop-down, select **4.2 (includes Apache Spark 2.3.1, Scala 11)**.
 -- MAGIC 1. Click **Create Cluster**.
 
@@ -20,12 +20,16 @@
 -- MAGIC %md
 -- MAGIC ## Demo
 -- MAGIC 
--- MAGIC - Display folder with databricks sample datasts
+-- MAGIC - Display folder with databricks sample Datasets
 -- MAGIC - create a table (airports) from a tab delimited file (airport-codes-na.txt)
 -- MAGIC - Display airports content
 -- MAGIC - Query airports: 
 -- MAGIC    - count airports by contry and state
 -- MAGIC    - compare airport count (US and CAN)
+
+-- COMMAND ----------
+
+-- MAGIC %fs ls "/databricks-datasets/"
 
 -- COMMAND ----------
 
@@ -49,6 +53,10 @@ DROP TABLE IF EXISTS airports;
 CREATE TABLE airports
   USING csv
   OPTIONS (path "/databricks-datasets/flights/airport-codes-na.txt", header "true", delimiter "\t")
+
+-- COMMAND ----------
+
+SHOW TABLES;
 
 -- COMMAND ----------
 
@@ -80,6 +88,11 @@ SELECT Country, COUNT(*) AS AirportCount FROM airports GROUP BY Country ORDER BY
 
 -- COMMAND ----------
 
+-- MAGIC %python
+-- MAGIC airports = spark.sql("SELECT * FROM airports")
+
+-- COMMAND ----------
+
 -- MAGIC %md 
 -- MAGIC ## Repeat the same operations using Python DataFrame API. 
 -- MAGIC This is a SQL notebook.
@@ -95,7 +108,6 @@ SELECT Country, COUNT(*) AS AirportCount FROM airports GROUP BY Country ORDER BY
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC 
 -- MAGIC airports = sqlContext.read.format("csv") \
 -- MAGIC   .options(header="true", inferSchema="true", delimiter="\t") \
 -- MAGIC   .load("/databricks-datasets/flights/airport-codes-na.txt")
@@ -104,8 +116,13 @@ SELECT Country, COUNT(*) AS AirportCount FROM airports GROUP BY Country ORDER BY
 
 -- MAGIC %md
 -- MAGIC ## Query airports DataFrame
--- MAGIC - count airports by contry and state
--- MAGIC - compare airport count (US and CAN) 
+-- MAGIC - Count airports by country and state
+-- MAGIC - Compare airport count (US and CAN) 
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Compare airport count (US and CAN) 
 
 -- COMMAND ----------
 
@@ -118,9 +135,25 @@ SELECT Country, COUNT(*) AS AirportCount FROM airports GROUP BY Country ORDER BY
 
 -- COMMAND ----------
 
+-- MAGIC %md
+-- MAGIC ### Count airports by country and state
+
+-- COMMAND ----------
+
 -- MAGIC %python
 -- MAGIC from pyspark.sql.functions import count
 -- MAGIC 
 -- MAGIC airportsByCountryAndState = airports.select("Country", "State").groupBy("Country", "State").agg(count("*").alias("AirportCount"))
 -- MAGIC 
 -- MAGIC display(airportsByCountryAndState.sort("AirportCount", ascending=False))
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Create Crosstab by Country / State
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC airportsCT = airports.crosstab("Country", "State")
+-- MAGIC display(airportsCT)
